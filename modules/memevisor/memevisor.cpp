9cc31e06ac6fa5c23a694b2dee68d0497175113c
Andrey Krasutsky <AndrezzzRus@gmail.com>
2022-06-18 16:53:52 +0300

    Test OpenCV func

--- modules/memevisor/memevisor.cpp
+++ modules/memevisor/memevisor.cpp
@@ -15,5 +15,11 @@ MemeVisor::~MemeVisor()
 
 void MemeVisor::test()
 {
+	std::string path = "C:/Qt/Projects/ImageViewer/Test Image.jpg";
+	Mat img = imread(path);
 
+	while (true) {
+		imshow("test", img);
+		waitKey(1);
+	}
 }
