92b1fa30673476f91d611aab7addc30d0a148f73
Stephan Kurnos <stephan.kurnos@gmail.com>
2022-06-21 21:15:05 +0300

    build_fix 2

--- build_win.bat
+++ build_win.bat
@@ -1,6 +1,6 @@
 mkdir .\build\3rdparty\opencv
 cd build\3rdparty\opencv
-cmake -G "Visual Studio 17 2022" -DBUILD_SHARED_LIBS=OFF -DBUILD_opencv_python3=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_opencv_apps=OFF -DOPENCV_EXTRA_MODULES_PATH=../../../3rdparty/opencv_contrib/modules ../../../3rdparty/opencv
+cmake -G "Visual Studio 17 2022" -DBUILD_opencv_python3=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_opencv_apps=OFF -DOPENCV_EXTRA_MODULES_PATH=../../../3rdparty/opencv_contrib/modules ../../../3rdparty/opencv
 cmake --build .  --config debug --target install -j4
 cd ..\..\
 cmake -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=Debug ../
