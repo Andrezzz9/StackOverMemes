adca051e03fbb27728da4661ca9b873805355fc8
Stephan Kurnos <stephan.kurnos@gmail.com>
2022-06-25 19:19:12 +0300

    Реализация логгирования в файл и консоль

--- modules/core/main.cpp
+++ modules/core/main.cpp
@@ -2,16 +2,29 @@
 #include "vkmanager.h"
 #include "memevisor.h"
 #include <QCoreApplication>
+#include <QScopedPointer>
+#include <QTextCodec>
 #include <iostream>
+
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
-	QCoreApplication app(argc, argv);
+	QScopedPointer<QCoreApplication> app = QScopedPointer(new QCoreApplication(argc, argv));
 
-	VkManager dataCollector;
-	dataCollector.getData();
-	
-	return app.exec();
+#ifdef Q_OS_WIN32
+	//setlocale(LC_ALL, "rus");
+	QTextCodec::setCodecForLocale(QTextCodec::codecForName("IBM 866"));
+#endif
+#ifdef Q_OS_LINUX
+	QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+#endif
+	// Инициализация лога
+	LogManager log;
+
+	qDebug() << "ОГО! Дебаг!";
+	qWarning() << " ~~ test предупреждение";
+
+	return app->exec();
 }
--- modules/logmanager/logmanager.cpp
+++ modules/logmanager/logmanager.cpp
@@ -1,9 +1,11 @@
-﻿#include "logmanager.h"
+#include "logmanager.h"
 
 #include <iostream>
 #include <QDateTime>
 #include <QString>
 #include <QFile>
+#include <QDir>
+#include <QTextCodec>
 
 using namespace std;
 
@@ -13,10 +15,17 @@ LogManager::LogManager(QString appName, QString logPath, QObject* parent): QObje
 {
     // Формирование названия файла
     QDateTime dateTime = QDateTime::currentDateTime();
-    QString fileName = QString("log_%1_%2").arg(appName).arg(dateTime.toString(Qt::ISODate));
+    QString fileName = QString("log_%1_%2.txt").arg(appName).arg(dateTime.toString("dd-MM-yyyy-hh-mm-ss"));
 
     // Полный путь до файла
     QString filePath = QString("%1/%2").arg(logPath).arg(fileName);
+
+    QDir dir;
+    if (!dir.mkpath(logPath))
+    {
+        qWarning() << "Не удалось создать директорию для записи логов. Логгирования не происходит";
+        return;
+    }
 
     // Проверка на валидность
     QFile logFile(filePath);
@@ -27,6 +36,7 @@ LogManager::LogManager(QString appName, QString logPath, QObject* parent): QObje
         m_logPath = filePath;
         qInstallMessageHandler(messageHandler);
         logFile.close();
+        cout << QString("Файл с логом: %1").arg(QFileInfo(logFile).absoluteFilePath()).toLocal8Bit().data() << endl;
     }
 }
 
@@ -39,9 +49,8 @@ void LogManager::messageHandler(const QtMsgType type, const QMessageLogContext& 
     QString txt;
     // Название функции с классом, берём только класс и саму функцию
     QRegExp rx("([\\w-]+::[\\w-]+)");
-    if (rx.indexIn(context.function) == -1) return;
+    //if (rx.indexIn(context.function) == -1) return;
     QString function = rx.cap(1);
-
     QString msgSep = (message.length() > 0) ? ">> " : "";
 
     switch (type) {
@@ -63,15 +72,22 @@ void LogManager::messageHandler(const QtMsgType type, const QMessageLogContext& 
         break;
     }
 
+    // Конечное сообщение
     QDateTime dateTime = QDateTime::currentDateTime();
     txt = QString("%1: %3").arg(dateTime.toString(Qt::ISODate)).arg(txt);
 
+    // Вывод в консоль
+    cout << txt.toLocal8Bit().data() << endl;
+
+    // Вывод в файл
+    if(m_logPath.isEmpty())
+        return;
     QFile outFile(m_logPath);
     if (outFile.open(QIODevice::WriteOnly | QIODevice::Append))
     {
         QTextStream ts(&outFile);
-        ts << txt << endl;
-        outFile.close();
+            ts.setCodec(QTextCodec::codecForName("UTF-8"));
+            ts << txt << endl;
+            outFile.close();
     }
-
 }
